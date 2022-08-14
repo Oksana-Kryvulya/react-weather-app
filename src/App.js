6e2f0a8132be2e4a42ddec02e6daf83e0180f293
Oksana-Kryvulya <roxann0791@gmail.com>
2022-08-14 12:08:37 +0300

    Added components and styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
-
-export default function App() {
-  return <div className="App">Hello</div>;
+import Search from "./Search";
+import BaseWindow from "./BaseWindow";
+import SourceLink from "./SourceLink";
+function App() {
+  return (
+    <div className="App">
+      <div className="bg">
+        <div className="container weather-app mt-xs-3 mt-sm-3 mt-md-5 mt-xl-5 pt-xl-5 pt-xxl-5">
+          <Search />
+          <BaseWindow />
+          <SourceLink />
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default App;
---
+++ src/SourceLink.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function SourceLink() {
+  return (
+    <div className="SourceLink">
+      <a
+        href="https://github.com/Oksana-Kryvulya/weather-react"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open-source code
+      </a>
+      <span> by Oksana Kryvulya</span>
+    </div>
+  );
+}
